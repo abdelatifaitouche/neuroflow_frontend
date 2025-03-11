ba839df19456d144d107132e9502461669121808
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-03-11 09:20:00 +0100

    fix : creation submit

--- src/pages/Procedures/CreateProcedurePage.jsx
+++ src/pages/Procedures/CreateProcedurePage.jsx
@@ -1,144 +1,135 @@
-
-
-
-import React , {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-import { createProcedure } from '@/services/proceduresService'
-import { Button } from '@/components/ui/button'
-import { getDepartement } from '@/services/departementService'
-import { Input } from '@/components/ui/input'
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { createProcedure } from "@/services/proceduresService";
+import { Button } from "@/components/ui/button";
+import { getDepartement } from "@/services/departementService";
+import { Input } from "@/components/ui/input";
 
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
 
-
-
 function CreateProcedurePage() {
+  const [ProcedureData, setProcedureData] = useState({
+    title: "",
+    description: "",
+    content: "",
+    department: 0,
+    status: "",
+    version: 0,
+  });
 
-    const [ProcedureData , setProcedureData] = useState({title : '' , description : '' , content : '' , department : 0  , status : '' , version : 0})
+  const [departements, setDepartements] = useState([]);
 
+  const editor = useCreateBlockNote({});
 
-    const [departements , setDepartements] = useState([])
+  const [content, setContent] = useState("");
 
-    
-    const editor = useCreateBlockNote({});
+  const fetchDepartements = async () => {
+    const departements_list = await getDepartement();
+    setDepartements(departements_list);
+  };
 
-    const [content , setContent] = useState("")
+  const onChange = async () => {
+    const html = await editor.blocksToHTMLLossy(editor.document);
 
+    setProcedureData((prev) => ({ ...prev, content: html }));
+  };
 
-    const fetchDepartements = async () =>{
-        const departements_list = await getDepartement();
-        setDepartements(departements_list)
-    }
+  useEffect(() => {
+    //fetchDepartements();
+    onChange();
+  }, []);
 
-    const onChange = async () => {
-      const html = await editor.blocksToHTMLLossy(editor.document)
-      
-      setProcedureData((prev)=>({...prev , content : html}))
-
-    }
-
-    useEffect(()=>{
-        //fetchDepartements();
-        onChange()
-     } , [])
-
-
-     const handleSubmit = (e)=>{
-        e.preventDefault();
-        console.log(ProcedureData)
-        createProcedure(ProcedureData);
-
-     }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(ProcedureData);
+    const response = await createProcedure(ProcedureData);
+  };
 
   return (
     <div>
-      <div className='flex justify-between'>
-        <div>
-
+      <div className="flex justify-between">
+        <div></div>
+        <div className="flex gap-2">
+          <Button className={"bg-gray-300"}>Save</Button>
         </div>
-        <div className='flex gap-2'>
-        <Button className={"bg-gray-300"}>Save</Button>
-        <Button type="submit">Create</Button>
-        </div>
-      
-
       </div>
-    <form onSubmit={handleSubmit} className='flex flex-col gap-2  h-[60vh]' >
-    
-    
-    <div className='flex gap-3 w-full h-full '>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2  h-[60vh]">
+        <div className="flex gap-3 w-full h-full ">
+          <div className="flex w-[20vw] flex-col gap-5 h-[100%]">
+            <Input
+              placeholder="Title"
+              onChange={(e) => {
+                setProcedureData((prev) => ({
+                  ...prev,
+                  title: e.target.value,
+                }));
+              }}
+            />
 
+            <Input
+              placeholder="description"
+              onChange={(e) => {
+                setProcedureData((prev) => ({
+                  ...prev,
+                  description: e.target.value,
+                }));
+              }}
+            />
 
-
-    <div className='flex w-[20vw] flex-col gap-5 h-[100%]'>
-    <Input placeholder='Title' onChange={(e)=>{
-        setProcedureData((prev)=>({...prev , title : e.target.value}))
-      }}/>
-
-<Input placeholder='description' onChange={(e)=>{
-        setProcedureData((prev)=>({...prev , description : e.target.value}))
-      }}/>
-
-
-
-
-      
-      <Select onValueChange={(value)=>{
-        console.log(value)
-        setProcedureData((prev)=>({...prev , department : value}))
-      }}>
-            <SelectTrigger className="w-[180px]">
+            <Select
+              onValueChange={(value) => {
+                console.log(value);
+                setProcedureData((prev) => ({ ...prev, department: value }));
+              }}
+            >
+              <SelectTrigger className="w-[180px]">
                 <SelectValue placeholder="Departements" />
-            </SelectTrigger>
-            <SelectContent>
-                
-            <SelectItem value="1">DG</SelectItem>
-            <SelectItem  value="2">RH</SelectItem>
-            </SelectContent>
-        </Select>
-<Input placeholder='status' onChange={(e)=>{
-        setProcedureData((prev)=>({...prev , status : e.target.value}))
-      }}/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1">DG</SelectItem>
+                <SelectItem value="2">RH</SelectItem>
+              </SelectContent>
+            </Select>
+            <Input
+              placeholder="status"
+              onChange={(e) => {
+                setProcedureData((prev) => ({
+                  ...prev,
+                  status: e.target.value,
+                }));
+              }}
+            />
 
-<Input placeholder='version' onChange={(e)=>{
-        setProcedureData((prev)=>({...prev , version : e.target.value}))
-      }}/>
-    
+            <Input
+              placeholder="version"
+              onChange={(e) => {
+                setProcedureData((prev) => ({
+                  ...prev,
+                  version: e.target.value,
+                }));
+              }}
+            />
+          </div>
+
+          <div className="flex-1">
+            <BlockNoteView editor={editor} onChange={onChange} />
+          </div>
+        </div>
+        <Button type="submit">Create</Button>
+
+      </form>
     </div>
-
-    <div className='flex-1'>
-    <BlockNoteView editor={editor} onChange={onChange} />
-    </div>
-    
-    </div>
-
-  
-    
-      
-
-
-
-
-
-    </form>    
-
-
-
-
-
-
-    </div>
-  )
+  );
 }
 
-export default CreateProcedurePage
+export default CreateProcedurePage;
