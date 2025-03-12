59dd427ed6a1a8311d88dc218773a9eaaad08dba
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-03-12 05:54:49 +0100

    UI FEAT : updated login page UI

--- src/components/ui/login-form.jsx
+++ src/components/ui/login-form.jsx
@@ -11,7 +11,77 @@ export function LoginForm(){
   let {login} = useContext(AuthContext)
 
   return (
-    <div className={cn("flex flex-col gap-6")}>
+    <div className={cn("flex flex-col items-center overflow-y-hidden")}>
+      <img
+              src="assets/images/logo_neuroflow.png"
+              alt="Image"
+              className="w-50"
+            />
+      <Card className="overflow-hidden h-[65vh] w-[25vw] bg-[#232323] text-gray-300 border-2 border-[#00FFCC]">
+        <CardContent className="grid p-2 h-full">
+          <form onSubmit={login} className="p-6 md:px-8 md:py-2 h-full">
+            <div className="flex flex-col gap-6 h-full justify-between">
+              <div className="flex flex-col items-center text-center gap-1">
+                <h1 className="text-3xl font-bold">WELCOME</h1>
+                <p className="text-balance text-gray-300 text-sm">
+                  Login to your NeuroFlow Account
+                </p>
+              </div>
+
+              
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name = 'email'
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input name='password' id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full bg-[#00FFCC] text-black">
+                Login
+              </Button>       
+            </div>
+          </form>
+          
+        </CardContent>
+      </Card>
+      <img
+              src="assets/images/neuroflow_logo_snd.png"
+              alt="Image"
+              className="w-50"
+            />
+    </div>
+  )
+}
+
+
+/**
+ * <div className="relative hidden bg-muted md:block">
+            <img
+              src="assets/images/LoginPageBG.jpg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+ */
+
+
+          /**
+           *  <div className={cn("flex flex-col gap-6")}>
       <Card className="overflow-hidden h-[70vh]">
         <CardContent className="grid p-2 md:grid-cols-2 h-full">
           <form onSubmit={login} className="p-6 md:p-8 h-full">
@@ -53,13 +123,7 @@ export function LoginForm(){
               
             </div>
           </form>
-          <div className="relative hidden bg-muted md:block">
-            <img
-              src="assets/images/LoginPageBG.jpg"
-              alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
-          </div>
+          
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
@@ -67,5 +131,4 @@ export function LoginForm(){
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
-  )
-}
+           */
